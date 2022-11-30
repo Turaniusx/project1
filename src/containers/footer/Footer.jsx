0913430c9f841ecf36ecab4860c6e6643399664f
Dimitar Turanov <dimitarturanov@gmail.com>
2022-11-30 14:07:29 +0100

    Fix: minor changes

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="gpt3__footer-copyright">
-        <p>© 2021 GPT-3. All rights reserved.</p>
+        <p>© 2022 Turanov GPT-3. All rights reserved.</p>
       </div>
     </div>
   );
