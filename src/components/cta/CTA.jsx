3b7bf685ea494e643c039a999450864685012ef5
Dimitar Turanov <dimitarturanov@gmail.com>
2022-11-30 11:57:52 +0100

    Fill CTA (Click-to-action) component + responsive

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt3__cta section__margin">
+      <div className="gpt3__cta-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
